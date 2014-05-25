01a5d64f410bf4772c0b4f0dbf6b0cd9004eca9c
Amanjeev Sethi <aj@amanjeev.com>
2014-05-24 20:57:04 -0400

    added both pages. checking if this works

--- grc.py
+++ grc.py
@@ -1,9 +1,45 @@
 import os
-from flask import Flask
+import sys
+from flask import Flask, render_template
+import copytext
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), './'))
 
 app = Flask(__name__)
+app.debug = True
+
 
 @app.route('/')
 def main():
-    return "hello world"
+    copy = copytext.Copy(os.path.join(PROJECT_ROOT, 'data/2014_bjp_manifesto.xlsx'))
+    context = {
+        'COPY' : copy,
+        'title': 'Home',
+        'page_title' : 'Manifesto items'
+    }
+    return render_template('index.html', **context)
+
+
+@app.route('/manifesto/<id>/')
+def manifesto_item(id):
+    copy = copytext.Copy(os.path.join(PROJECT_ROOT, 'data/2014_bjp_manifesto.xlsx'))
+    sheet = copy['main_manifesto_items']
+    context = {
+        'COPY' : copy,
+        'row' : sheet[int(id) - 1]
+    }
+    return render_template('manifesto_item.html', **context)
+
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+@app.route('/current-manifesto/')
+def current_manifesto():
+    return render_template('current_manifesto.html')
+
+
+#to run Flask locally
+if __name__ == '__main__':
+    app.run()
 
