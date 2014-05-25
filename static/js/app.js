4642eb7feb983f80b38c59fbcd242e344ca4d488
Amanjeev Sethi <aj@amanjeev.com>
2014-05-25 10:23:29 -0400

    added filtering

---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+$.expr[":"].contains = $.expr.createPseudo(function(arg) {
+    return function( elem ) {
+        return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
+    };
+});
+
+(function($){
+    var list = $("#manifesto_items_list");
+    var text = $("#qfilter_id");
+    text.bind("input keypress change", function(e){
+        var input_value = $(this).val();
+        if (input_value != '') {
+            list.children("li").hide();
+            foundLi = $('li:contains("' + input_value + '")');
+            foundLi.show();
+            $("#no_result_message").show();
+        } else {
+            list.children("li").show();
+            $("#no_result_message").hide();
+        }
+    });
+})(jQuery)
